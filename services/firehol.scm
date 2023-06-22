98a45fa1c0f0bcc680f0f25270e144bb36f1fc37
TinHead <razlix77@gmail.com>
2023-06-22 14:44:16 +0300

    test config

--- services/firehol.scm
+++ services/firehol.scm
@@ -6,7 +6,7 @@
   #:export (firehol-configuration))
 
 (define-record-type* <firehol-configuration>
-  firehol-confguration make-firehol-configuration
+  firehol-configuration make-firehol-configuration
   firehol-configuration?
   (version firehol-configuration-version (default 6))
   )
