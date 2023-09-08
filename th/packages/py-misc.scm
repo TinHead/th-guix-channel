dc3e611c66f05888bc453f7f4a7296e1b049f1d8
TinHead <razlix77@gmail.com>
2023-09-08 19:51:18 +0300

    misc python packages

--- th/packages/py-misc.scm
+++ th/packages/py-misc.scm
@@ -5,6 +5,7 @@
 #:use-module (guix build-system python)
 #:use-module (guix build-system pyproject)
 #:use-module (gnu packages check)
+#:use-module (gnu packages python-xyz)
 #:use-module (guix licenses))
 
 (define-public python-atomicwrites-homeassistant 
