1f0349e5a9878bca484c9f2a3bd06041fe1020b8
Razvan <razvan.lixandru@essensys.ro>
2023-09-11 16:10:25 +0300

    env not working

--- th/packages/firehol.scm
+++ th/packages/firehol.scm
@@ -28,7 +28,18 @@
                              "firehol-uname.patch"))))  
   (build-system gnu-build-system)
   (arguments
-  `(#:configure-flags '("--localstatedir=/var")))
+  `(#:configure-flags '("--localstatedir=/var")
+   (#:phases
+    modify-phases %standard-phases
+     (add-after 'install 'wrap-program
+     (lambda* (#:key inputs outputs #:allow-other-keys)
+             (let ((out (assoc-ref outputs "out")))
+               (wrap-program (string-append out "/sbin/firehol")
+                 `("PATH" ":" prefix
+                   ,(map (lambda (input)
+                           (string-append (assoc-ref inputs input) "/bin"))
+                         '("readlink"
+                           "dirname")))))))))))
   (propagated-inputs 
 	(list util-linux
         coreutils 
