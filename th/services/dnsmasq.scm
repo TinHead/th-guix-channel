a3b6acac300988f1cad5b2530bc7866b9fe4675a
Razvan <razvan.lixandru@essensys.ro>
2023-11-09 12:20:29 +0200

    add dnsmasq service

--- th/services/dnsmasq.scm
+++ th/services/dnsmasq.scm
@@ -190,7 +190,7 @@
     (provision '(dnsmasq))
     (requirement '(networking))
     (start #~(make-forkexec-constructor 
-              (list #$(file-append dnsmasq "/sbin/dnsmasq") "--keep-in-foreground" "--pid-file=/run/dnsmasq.pid" $(string-append "--config-file=" (serialize-dnsmasq-config config)))
+              (list #$(file-append dnsmasq "/sbin/dnsmasq") "--keep-in-foreground" "--pid-file=/run/dnsmasq.pid" (string-append "--config-file=" (serialize-dnsmasq-config config)))
               #:pid-file "/run/dnsmasq.pid")) 
     (stop  #~(make-kill-destructor))
     )))
