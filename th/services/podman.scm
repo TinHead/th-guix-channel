df8c7710de39626c817187a75c139e475dacfea2
TinHead <razlix77@gmail.com>
2024-07-20 13:54:35 +0300

    fix policy dir

--- th/services/podman.scm
+++ th/services/podman.scm
@@ -203,8 +203,8 @@
               "podman-container:16777216:65536")
              "\n"))
         
-        (mkdir-p "/var/podman/.config/containers")
-        (plain-file "/var/podman/.config/containers/policy.json" podman-containers-policy))))
+        (mkdir-p "/etc/containers")
+        (plain-file "/etc/containers/policy.json" podman-containers-policy))))
 
 (define %oci-container-accounts
   (list (user-group
