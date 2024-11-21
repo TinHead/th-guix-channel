ce30d9570933ac29dc2df1d2dde1b5c4ed2d8d54
TinHead <razlix77@gmail.com>
2024-11-21 15:26:40 +0200

    guile-fibers without patch

--- th/packages/guile-lsp.scm
+++ th/packages/guile-lsp.scm
@@ -16,6 +16,84 @@
 
 (define %source-dir (getcwd))
 
+(define-public guile-fibers
+  (package
+    (name "guile-fibers")
+    (version "1.3.1")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/wingo/fibers")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0wvdi4l58f9a5c9wi3cdc9l1bniscsixb6w2zj86mch7j7j814lc"))
+              (patches
+               (search-patches "guile-fibers-libevent-32-bit.patch"
+                               "guile-fibers-libevent-timeout.patch"))))
+    (build-system gnu-build-system)
+    (arguments
+     (list #:make-flags
+           #~(list "GUILE_AUTO_COMPILE=0")
+           #:phases
+           (if (target-x86-64?)
+               #~%standard-phases
+               #~(modify-phases %standard-phases
+                   (add-before 'check 'disable-some-tests
+                     (lambda _
+                       ;; This test can take more than an hour on some systems.
+                       (substitute* "tests/basic.scm"
+                         ((".*spawn-fiber loop-to-1e4.*") ""))
+
+                       ;; These tests can take more than an hour and/or segfault.
+                       (substitute* "Makefile"
+                         (("tests/speedup.scm") ""))
+
+                       (when #$(target-aarch64?)
+                         ;; The tests below have issues on aarch64 systems.
+                         ;; They pass on an Apple M1 but take a very long time
+                         ;; on a Hetzner aarch64 VM.  Skip them.
+                         (substitute* "tests/basic.scm"
+                           ((".*spawn-fiber-chain 5000000.*") ""))
+                         (substitute* "tests/channels.scm"
+                           ((".*assert-run-fibers-terminates .*pingpong.*") "")))))
+                   #$@(if (%current-target-system)
+                          ; #~((add-after 'unpack 'apply-cross-build-fix-patch
+                               ; (lambda _
+                                 ; (let ((patch-file
+                                        ; #$(local-file
+                                           ; (search-patch
+                                            ; "guile-fibers-cross-build-fix.patch"))))
+                                   ; (invoke "patch" "--force" "-p1" "-i"
+                                           ; patch-file)))))
+                          #~())))))
+    (native-inputs
+     (list texinfo pkg-config autoconf-2.71 automake libtool
+           guile-3.0            ;for 'guild compile
+           ;; Gettext brings 'AC_LIB_LINKFLAGS_FROM_LIBS'
+           gettext-minimal))
+    (inputs
+     (append (list guile-3.0)           ;for libguile-3.0.so
+             (if (target-hurd?)
+                 (list libevent)
+                 '())))
+    (synopsis "Lightweight concurrency facility for Guile")
+    (description
+     "Fibers is a Guile library that implements a a lightweight concurrency
+facility, inspired by systems like Concurrent ML, Go, and Erlang.  A fiber is
+like a \"goroutine\" from the Go language: a lightweight thread-like
+abstraction.  Systems built with Fibers can scale up to millions of concurrent
+fibers, tens of thousands of concurrent socket connections, and many parallel
+cores.  The Fibers library also provides Concurrent ML-like channels for
+communication between fibers.
+
+Note that Fibers makes use of some Guile 2.1/2.2-specific features and
+is not available for Guile 2.0.")
+    (home-page "https://github.com/wingo/fibers")
+    (properties '((upstream-name . "fibers")))
+    (license license:lgpl3+)))
+
 (define-public guile-json-rpc
   (let ((version "0.4.5")
         (revision "a")
